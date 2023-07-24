f59c7f067a8280ace01f5723f9ae37a5681cc962
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-07-24 23:53:45 +0200

    changed img tag on next/image Image

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import styles from './icon.module.css'
+import Image from 'next/image'
 
 const Icon = ({ icon, size }) => {
   const iconUrl = `/icons/${icon}.svg`
   return (
-    <img
+    <Image
       src={iconUrl}
       className={styles.icon}
       alt={icon}
-      style={{ width: size, height: size }}
+      width={size}
+      height={size}
     />
   )
 }
