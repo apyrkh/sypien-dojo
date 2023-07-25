8cf3d6edbf403b49d285ab497977ef5d8a0b69bc
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-07-25 17:18:35 +0200

    added animation file and prop to Icon component so we can choose animation with a prop

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './icon.module.css'
 import Image from 'next/image'
+import '@/app/animations.css'
 
-const Icon = ({ icon, size }) => {
+const Icon = ({ icon, size, animation }) => {
   const iconUrl = `/icons/${icon}.svg`
   return (
     <Image
       src={iconUrl}
-      className={styles.icon}
+      className={`${styles.icon} ${animation}`}
       alt={icon}
       width={size}
       height={size}
