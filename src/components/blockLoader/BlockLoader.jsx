1f9795958021041e78d99d22a6e1b58a3d7b7b5c
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-04 01:01:44 +0200

    loader changes

--- src/components/blockLoader/BlockLoader.jsx
+++ src/components/blockLoader/BlockLoader.jsx
@@ -3,22 +3,19 @@ import React from 'react'
 import styles from './blockLoader.module.css'
 import { useLoader } from '@/context/LoaderContext.jsx'
 import { createPortal } from 'react-dom'
+// import { Portal } from 'next/dist/client/portal'
 
 const BlockLoader = () => {
   const { isLoading } = useLoader()
   if (!isLoading) return null
   console.log(isLoading, document.body)
-  return (
-    <div>
-      {createPortal(
-        <div className={styles.modal}>
-          <div className={styles.overlay}>
-            <div className={styles.content}>loader</div>
-          </div>
-        </div>,
-        document.body
-      )}
-    </div>
+  return createPortal(
+    <div className={styles.modal}>
+      <div className={styles.overlay}>
+        <div className={styles.content}>loader</div>
+      </div>
+    </div>,
+    document.body
   )
 }
 
