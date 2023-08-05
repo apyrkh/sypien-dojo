2635199dcc5a5fb69a8146eea7360d957a8cca75
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-05 20:10:38 +0200

    changed portal implementation with better solution(createPortal => Portal tag)

--- src/components/blockLoader/BlockLoader.jsx
+++ src/components/blockLoader/BlockLoader.jsx
@@ -2,20 +2,20 @@
 import React from 'react'
 import styles from './blockLoader.module.css'
 import { useLoader } from '@/context/LoaderContext.jsx'
-import { createPortal } from 'react-dom'
-// import { Portal } from 'next/dist/client/portal'
+// import { createPortal } from 'react-dom'
+import { Portal } from 'next/dist/client/portal'
 
 const BlockLoader = () => {
   const { isLoading } = useLoader()
   if (!isLoading) return null
-  console.log(isLoading, document.body)
-  return createPortal(
-    <div className={styles.modal}>
-      <div className={styles.overlay}>
-        <div className={styles.content}>loader</div>
+  return (
+    <Portal className={styles.portal}>
+      <div className={styles.modal}>
+        <div className={styles.overlay}>
+          <div className={styles.content}>loader</div>
+        </div>
       </div>
-    </div>,
-    document.body
+    </Portal>
   )
 }
 
