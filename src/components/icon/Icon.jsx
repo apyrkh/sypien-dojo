3f41ce3fd42211c0601317e38acebf551031c71c
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-06 13:44:39 +0200

    resolved icon changes before merge

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 import styles from './icon.module.css'
 import '@/app/animations.css'
 
-const Icon = ({ icon, size, animation, color }) => {
+const Icon = ({ icon, size, animation }) => {
   return (
-    <svg
-      className={`${styles.icon} ${animation}`}
-      width={size}
-      height={size}
-      stroke={color}
-    >
+    <svg className={`${styles.icon} ${animation}`} width={size} height={size}>
       <use href={`/icons/feather-sprite.svg#${icon}`} />
     </svg>
   )
