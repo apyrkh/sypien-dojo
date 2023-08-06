eec0bee8c05cec9271e7287a03d92f95ef256f81
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-06 13:17:24 +0200

    deleted unnecessary lines

--- src/components/blockLoader/BlockLoader.jsx
+++ src/components/blockLoader/BlockLoader.jsx
@@ -2,17 +2,16 @@
 import React from 'react'
 import styles from './blockLoader.module.css'
 import { useLoader } from '@/context/LoaderContext.jsx'
-// import { createPortal } from 'react-dom'
 import { Portal } from 'next/dist/client/portal'
 
 const BlockLoader = () => {
   const { isLoading } = useLoader()
   if (!isLoading) return null
   return (
-    <Portal className={styles.portal}>
+    <Portal>
       <div className={styles.modal}>
         <div className={styles.overlay}>
-          <div className={styles.content}>loader</div>
+          <div className={styles.content}>Loading...</div>
         </div>
       </div>
     </Portal>
