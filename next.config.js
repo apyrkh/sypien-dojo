f21a424e75838dc9ddb934e3eceb3b0ce87d05a4
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-12 19:53:34 +0200

    implemented dropdown menu and other styles for user container

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['scontent-waw1-1.xx.fbcdn.net', 'cdn.discordapp.com'],
+    domains: [
+      'scontent-waw1-1.xx.fbcdn.net',
+      'cdn.discordapp.com',
+      'platform-lookaside.fbsbx.com',
+    ],
   },
 }
 
