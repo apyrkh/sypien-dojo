9f50779b0cdab71e178089e8fbc65f9e18e76fe8
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-17 11:18:05 +0200

    resolved requested changes for PR

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -7,32 +7,29 @@ import { formatDate } from '@/utils/formatters'
 export default async function AdminPage() {
   const session = await getServerSession(authOptions)
   const pages = session ? await getPages() : null
-  console.log(pages)
   return (
     <div>
       <h1>Admin page</h1>
       <br />
-      {/* {session && <pre>{JSON.stringify(pages, null, 2)}</pre>} */}
+
       {session && (
         <div className={styles.tableContainer}>
           <h2 className={styles.h2}>Supported Facebook pages</h2>
           <table className={styles.table}>
-            <tr>
-              <th className={styles.tableHead}>Page Id</th>
-              <th className={styles.tableHead}>Page Name</th>
-              <th className={styles.tableHead}>Access Token</th>
-              <th className={styles.tableHead}>Expires at</th>
-              <th className={styles.tableHead}>Last Synchronized At</th>
+            <tr className={styles.mainRow}>
+              <th>Page Id</th>
+              <th>Page Name</th>
+              <th>Access Token</th>
+              <th>Expires at</th>
+              <th>Last Synchronized At</th>
             </tr>
             {pages.map((e) => (
-              <tr key={e.id}>
-                <td className={styles.tableData}>{e.pageId}</td>
-                <td className={styles.tableData}>{e.pageName}</td>
-                <td className={`${styles.tableData}`}>
-                  {'***' + e.accessToken.slice(-3)}
-                </td>
-                <td className={styles.tableData}>{formatDate(e.expiresAt)}</td>
-                <td className={styles.tableData}>
+              <tr key={e.id} className={styles.dataRow}>
+                <td>{e.pageId}</td>
+                <td>{e.pageName}</td>
+                <td>{'***' + e.accessToken.slice(-3)}</td>
+                <td>{formatDate(e.expiresAt)}</td>
+                <td>
                   {e.lastSynchronizedAt ? e.lastSynchronizedAt : 'Unknown'}
                 </td>
               </tr>
