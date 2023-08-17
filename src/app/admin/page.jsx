0e9a89ca3d9f1ed75b5dbb92be75419584e8f618
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2023-08-18 01:33:38 +0200

    Fixed table with fb pages on the admin page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -16,24 +16,28 @@ export default async function AdminPage() {
         <div className={styles.tableContainer}>
           <h2 className={styles.h2}>Supported Facebook pages</h2>
           <table className={styles.table}>
-            <tr className={styles.mainRow}>
-              <th>Page Id</th>
-              <th>Page Name</th>
-              <th>Access Token</th>
-              <th>Expires at</th>
-              <th>Last Synchronized At</th>
-            </tr>
-            {pages.map((e) => (
-              <tr key={e.id} className={styles.dataRow}>
-                <td>{e.pageId}</td>
-                <td>{e.pageName}</td>
-                <td>{'***' + e.accessToken.slice(-3)}</td>
-                <td>{formatDate(e.expiresAt)}</td>
-                <td>
-                  {e.lastSynchronizedAt ? e.lastSynchronizedAt : 'Unknown'}
-                </td>
+            <thead>
+              <tr className={styles.mainRow}>
+                <th>Page Id</th>
+                <th>Page Name</th>
+                <th>Access Token</th>
+                <th>Expires at</th>
+                <th>Last Synchronized At</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {pages.map((e) => (
+                <tr key={e.id} className={styles.dataRow}>
+                  <td>{e.pageId}</td>
+                  <td>{e.pageName}</td>
+                  <td>{'***' + e.accessToken.slice(-3)}</td>
+                  <td>{formatDate(e.expiresAt)}</td>
+                  <td>
+                    {e.lastSynchronizedAt ? e.lastSynchronizedAt : 'Unknown'}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       )}
