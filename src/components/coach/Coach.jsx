3e9452c3d7cb30353f1f5d5c4b40de523b52e285
Rostyslav Bezverkhnii <106315772+bezverkhnii@users.noreply.github.com>
2023-09-10 19:36:49 +0200

    implemented requested changes (#24)
    Co-authored-by: Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>

--- src/components/coach/Coach.jsx
+++ src/components/coach/Coach.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import ColorBar from '../colorBar/ColorBar'
 import styles from './coach.module.css'
 
-const Coach = ({ name, description, direction }) => {
+const Coach = ({
+  name,
+  classification,
+  titleDescription,
+  titles,
+  description,
+  direction,
+}) => {
   return (
     <div
       className={styles.container}
@@ -11,7 +18,20 @@ const Coach = ({ name, description, direction }) => {
       <div className={styles.background}>{/* photo */}</div>
       <div className={styles.textContainer}>
         <h3 className={styles.name}>{name}</h3>
+        {classification.map((item, index) => (
+          <p className={styles.classification} key={index}>
+            {item}
+          </p>
+        ))}
         <ColorBar />
+        <p className={styles.desc}>{titleDescription}</p>
+        <ul className={styles.titlesList}>
+          {titles.map((title, index) => (
+            <li key={index} className={styles.title}>
+              {title}
+            </li>
+          ))}
+        </ul>
         <p className={styles.desc}>{description}</p>
       </div>
     </div>
